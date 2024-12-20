b3f8f235b81825e9992d31bd10d62c689ae38667
vimal varun <vimalvarun98@gmail.com>
2024-12-20 22:58:49 +0530

    Exceptions

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging  # Importing your logging configuration
+from src.logger import logging  
 
 def error_message_detail(error, error_detail: sys):
     try:
@@ -22,6 +22,12 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
+if __name__=="__main__":
 
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divided by Zero")
+        raise CustomException(e, sys)
 
 
